294d5742b041aa7c70e967d9f5d78b972cbc8c16
smileclj <842705148@qq.com>
2015-11-18 16:43:43 +0800

    xx

--- src/main/java/com/clj/panda/service/TestService.java
+++ src/main/java/com/clj/panda/service/TestService.java
@@ -1,10 +1,12 @@
 package com.clj.panda.service;
 
 import com.clj.panda.model.entity.test.TestStudent;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by lao on 2015/9/28.
  */
+@Service
 public interface TestService {
 
 }
--- src/main/java/com/clj/panda/web/TestListener.java
+++ src/main/java/com/clj/panda/web/TestListener.java
@@ -1,6 +1,9 @@
 package com.clj.panda.web;
 
+import com.clj.panda.mapper.test.TestStudentMapper;
+import com.clj.panda.service.TestService;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -11,6 +14,9 @@ import javax.servlet.ServletContextListener;
 public class TestListener implements ServletContextListener{
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
+        TestStudentMapper testStudentMapper = (TestStudentMapper)WebApplicationContextUtils.getWebApplicationContext(servletContextEvent.getServletContext()).getBean("testStudentMapper");
+        System.out.println("容器启动了");
+        System.out.println(testStudentMapper);
 
     }
 
